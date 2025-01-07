44a792cb4a13db9802cb7faf714ae07f249c4e3e
Anthony <anthonyrov@gmail.com>
2025-01-07 21:59:40 +0100

    add: Suspense on useSearchParams

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import { useState, Suspense } from "react";
 import SearchBar from "@/components/SearchBar";
 import SearchResult from "@/components/SearchResult";
 import { Recipe } from "@/types";
+import { Loader } from "lucide-react";
 
 export default function Home() {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
@@ -16,26 +17,34 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen t-24 p-4 md:p-8 bg-background-light dark:bg-background-dark">
-      <div className="max-w-6xl mx-auto">
-        <p className="text-center text-secondary-light/70 dark:text-secondary-dark/70 mb-12 max-w-xl mx-auto">
-          Pick the ingredients you have and we&apos;ll find the perfect recipe for you!
-        </p>
-
-        <div className="p-0 md:p-8 rounded-2xl">
-          <SearchBar handleSetRecipes={handleSetRecipes} setIsSearching={setIsSearching} />
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center bg-background-light dark:bg-background-dark">
+          <Loader className="text-primary-dark animate-spin size-10" />
         </div>
+      }
+    >
+      <div className="min-h-screen t-24 p-4 md:p-8 bg-background-light dark:bg-background-dark">
+        <div className="max-w-6xl mx-auto">
+          <p className="text-center text-secondary-light/70 dark:text-secondary-dark/70 mb-12 max-w-xl mx-auto">
+            Pick the ingredients you have and we&apos;ll find the perfect recipe for you!
+          </p>
 
-        {error && <div className="text-red-500 text-center mt-4">{error}</div>}
-
-        {isSearching ? (
-          <div className="text-center mt-12">
-            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary-light dark:border-primary-dark mx-auto"></div>
+          <div className="p-0 md:p-8 rounded-2xl">
+            <SearchBar handleSetRecipes={handleSetRecipes} setIsSearching={setIsSearching} />
           </div>
-        ) : (
-          recipes.length > 0 && <SearchResult recipes={recipes} />
-        )}
+
+          {error && <div className="text-red-500 text-center mt-4">{error}</div>}
+
+          {isSearching ? (
+            <div className="text-center mt-12">
+              <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary-light dark:border-primary-dark mx-auto"></div>
+            </div>
+          ) : (
+            recipes.length > 0 && <SearchResult recipes={recipes} />
+          )}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
